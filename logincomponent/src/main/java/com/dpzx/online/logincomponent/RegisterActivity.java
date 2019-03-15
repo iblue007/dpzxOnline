d600b0c215e3a9c680db12c3c5b9a77af1267971
iblue007 <744639775@qq.com>
2019-03-15 11:09:16 +0800

    注册页面修改

--- logincomponent/src/main/java/com/dpzx/online/logincomponent/RegisterActivity.java
+++ logincomponent/src/main/java/com/dpzx/online/logincomponent/RegisterActivity.java
@@ -26,6 +26,7 @@ import com.dpzx.online.baselib.utils.MessageUtils;
 import com.dpzx.online.baselib.utils.ScreenUtil;
 import com.dpzx.online.baselib.utils.ThreadUtil;
 import com.dpzx.online.corlib.app.BaseActivity;
+import com.dpzx.online.logincomponent.widget.PhoneCodeView;
 
 
 /**
@@ -36,6 +37,8 @@ public class RegisterActivity extends BaseActivity implements View.OnClickListen
     private TextView titleTv;
     private RelativeLayout commonBackRl;
     private ImageView oneStepIv;
+    private ImageView twoStepIv;
+    private ImageView threeStepIv;
     private View lineView;
     private TextView oneStepTv;
     private LinearLayout oneStepLL;
@@ -43,6 +46,10 @@ public class RegisterActivity extends BaseActivity implements View.OnClickListen
     private EditText registerPhoneEt;
     private ImageView registerPhoneClearIv;
     private TextView registerCodeTv;
+    private PhoneCodeView phoneCodeView;
+    private LinearLayout oneStepContentLL;
+    private LinearLayout twoStepContentLL;
+    private LinearLayout threeStepContentLL;
 
     @Override
     protected void setLayout() {
@@ -53,6 +60,8 @@ public class RegisterActivity extends BaseActivity implements View.OnClickListen
         commonBackRl = findViewById(R.id.common_back_rl);
         titleTv.setText("注册");
         oneStepIv = findViewById(R.id.login_register_1_iv);
+        twoStepIv = findViewById(R.id.login_register_2_iv);
+        threeStepIv = findViewById(R.id.login_register_3_iv);
         lineView = findViewById(R.id.login_register_line);
         oneStepTv = findViewById(R.id.login_register_1_tv);
         oneStepLL = findViewById(R.id.login_register_1_ll);
@@ -60,9 +69,16 @@ public class RegisterActivity extends BaseActivity implements View.OnClickListen
         registerPhoneEt = findViewById(R.id.login_register_phone_et);
         registerPhoneClearIv = findViewById(R.id.login_register_phone_clear);
         registerCodeTv = findViewById(R.id.login_register_code_tv);
+        oneStepContentLL = findViewById(R.id.login_register_onestep_ll);
+        twoStepContentLL = findViewById(R.id.login_register_twostep_ll);
+        threeStepContentLL = findViewById(R.id.login_register_threestep_ll);
+
+
+        phoneCodeView = findViewById(R.id.login_register_phonecode_view);
 
         registerCodeTv.setOnClickListener(this);
         oneStepIv.setBackgroundResource(R.drawable.login_register_1_enable);
+        oneStepContentLL.setVisibility(View.VISIBLE);
         oneStepIv.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
@@ -106,6 +122,20 @@ public class RegisterActivity extends BaseActivity implements View.OnClickListen
 
             }
         });
+        phoneCodeView.setOnInputListener(new PhoneCodeView.OnInputListener() {
+            @Override
+            public void onSucess(String code) {
+                Log.e("======","======onSucess");
+                twoStepIv.setBackgroundResource(R.drawable.login_register_complete);
+                twoStepContentLL.setVisibility(View.GONE);
+                threeStepContentLL.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onInput() {
+                Log.e("======","======onInput");
+            }
+        });
     }
 
     @Override
@@ -116,33 +146,36 @@ public class RegisterActivity extends BaseActivity implements View.OnClickListen
                 MessageUtils.show(getApplicationContext(),"常用号码不能为空");
                 return;
             }
-            ThreadUtil.executeMore(new Runnable() {
-                @Override
-                public void run() {
-                    ServerResult<VideoPaperBean> phoneExit = NetApiUtil.getPhoneExit(phonestr);
-                    boolean requestOK = phoneExit.getCsResult().isRequestOK();
-                    boolean datas = phoneExit.getCsResult().getDatas();
-                    if(!datas){
-
-                        ServerResult<VideoPaperBean> videoPaperBeanServerResult = NetApiUtil.postPhonecode(phonestr);
-                        String resultMessage = videoPaperBeanServerResult.getCsResult().getResultMessage();
-                        if(!TextUtils.isEmpty(resultMessage)){
-                            NetApiUtil.postUserRegister(phonestr,"123456",resultMessage,0+"");
-                        }
-                    }
-//                    if (!requestOK) {
-//                        Global.runInMainThread(new Runnable() {
-//                            @Override
-//                            public void run() {
-//                                ServerResultHeader csResult = phoneExit.getCsResult();
-//                                String resultMessage = csResult.getResultMessage();
-//                                MessageUtils.show(getApplicationContext(),resultMessage);
-//                                Log.e("======", "======" + resultMessage + "--requestOK:" + requestOK+"--datas:"+datas);
-//                            }
-//                        });
+            oneStepIv.setBackgroundResource(R.drawable.login_register_complete);
+            oneStepContentLL.setVisibility(View.GONE);
+            twoStepContentLL.setVisibility(View.VISIBLE);
+//            ThreadUtil.executeMore(new Runnable() {
+//                @Override
+//                public void run() {
+//                    ServerResult<VideoPaperBean> phoneExit = NetApiUtil.getPhoneExit(phonestr);
+//                    boolean requestOK = phoneExit.getCsResult().isRequestOK();
+//                    boolean datas = phoneExit.getCsResult().getDatas();
+//                    if(!datas){
+//
+//                        ServerResult<VideoPaperBean> videoPaperBeanServerResult = NetApiUtil.postPhonecode(phonestr);
+//                        String resultMessage = videoPaperBeanServerResult.getCsResult().getResultMessage();
+//                        if(!TextUtils.isEmpty(resultMessage)){
+//                            NetApiUtil.postUserRegister(phonestr,"123456",resultMessage,0+"");
+//                        }
 //                    }
-                }
-            });
+////                    if (!requestOK) {
+////                        Global.runInMainThread(new Runnable() {
+////                            @Override
+////                            public void run() {
+////                                ServerResultHeader csResult = phoneExit.getCsResult();
+////                                String resultMessage = csResult.getResultMessage();
+////                                MessageUtils.show(getApplicationContext(),resultMessage);
+////                                Log.e("======", "======" + resultMessage + "--requestOK:" + requestOK+"--datas:"+datas);
+////                            }
+////                        });
+////                    }
+//                }
+//            });
         }
     }
 }
